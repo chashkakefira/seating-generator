7ecb6cc425e899e7f0b4bc6fd35d39bf6cf645fb
chashkakefira <danieldzen@yandex.ru>
2026-01-14 14:59:39 +0300

    remove unused bootstrap-vue-next import

--- presentation/setup/main.ts
+++
@@ -1,8 +0,0 @@
-import { defineAppSetup } from '@slidev/types'
-import { createBootstrap } from 'bootstrap-vue-next'
-
-import 'bootstrap-vue-next/dist/bootstrap-vue-next.css'
-
-export default defineAppSetup(({ app }) => {
-  app.use(createBootstrap())
-})
